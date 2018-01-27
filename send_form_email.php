f55c686488c04bb4e6920efb4a33ddd4ef733d87
taiwan-little-eats <30437318+taiwan-little-eats@users.noreply.github.com>
2018-01-27 15:34:35 -0600

    Update send_form_email.php

--- send_form_email.php
+++ send_form_email.php
@@ -29,36 +29,19 @@ if(isset($_POST['email'])) {
     $oolong = $_POST['oolong'];
     $pouchong = $_POST['pouchong'];
     $taiwanese = $_POST['taiwanese'];
-    $jasmine-milk = $_POST['jasmine-milk'];
-    $oolong-milk = $_POST['oolong-milk'];
-    $pouchong-milk = $_POST['pouchong-milk'];
+    $jasmine_milk = $_POST['jasmine-milk'];
+    $oolong_milk = $_POST['oolong-milk'];
+    $pouchong_milk = $_POST['pouchong-milk'];
     $chicken = $_POST['chicken'];
     $pork = $_POST['pork'];
     $popcorn = $_POST['popcorn'];
     $beef = $_POST['beef'];
     $tofu = $_POST['tofu'];
  
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$first_name)) {
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-  }
- 
-  if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-  }
+   
  
  
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
+
  
     $email_message = "Order details below.\n\n";
  
@@ -74,15 +57,15 @@ if(isset($_POST['email'])) {
     $email_message .= "Last Name : ".clean_string($last_name)."\n";
     $email_message .= "Email : ".clean_string($email_from)."\n";
     $email_message .= "Telephone : ".clean_string($telephone-number)."\n";
-    $email_message .= "Comments : ".clean_string($date-time)."\n";
+    $email_message .= "Comments : ".clean_string($date_time)."\n";
     $email_message .= "Assam Black Tea : ".clean_string($assam)."\n";
     $email_message .= "Jasmine Green Tea : ".clean_string($jasmine)."\n";
     $email_message .= "Roasted Oolong Tea : ".clean_string($oolong)."\n";
     $email_message .= "Pouchong Tea : ".clean_string($pouchong)."\n";
     $email_message .= "Taiwanese Style Milk Tea : ".clean_string($taiwanese)."\n";
-    $email_message .= "Jasmine Green Milk Tea : ".clean_string($jasmine-milk)."\n"; 
-    $email_message .= "Roasted Oolong Milk Tea : ".clean_string($oolong-milk)."\n";  
-    $email_message .= "Pouchong Milk Tea : ".clean_string($pouchong-milk)."\n";
+    $email_message .= "Jasmine Green Milk Tea : ".clean_string($jasmine_milk)."\n"; 
+    $email_message .= "Roasted Oolong Milk Tea : ".clean_string($oolong_milk)."\n";  
+    $email_message .= "Pouchong Milk Tea : ".clean_string($pouchong_milk)."\n";
     $email_message .= "Fried Chicken Cutlet over Rice : ".clean_string($chicken)."\n";  
     $email_message .= "Fried Pork Rib over Rice : ".clean_string($pork)."\n";
     $email_message .= "Popcorn Chicken over Rice : ".clean_string($popcorn)."\n";
